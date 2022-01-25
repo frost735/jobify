fbeea4631e3208d1585d95f1d80913c5c61eb80d
Alexey Kovalenko <97091814+frost735@users.noreply.github.com>
2022-01-26 01:03:39 +0300

    ~

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -2,7 +2,24 @@ import logo from '../assets/logo.svg'
 import main from '../assets/main.svg'
 
 function Landing() {
-  return <h4>Landing Page</h4>;
+  return (
+    <main>
+        <nav>
+            <img src={logo} alt='jobify' className='logo'/>
+        </nav>
+        <div className='container page'>
+            {/* info */}
+            <div className='info'>
+                <h1>
+                    job <span>tracking</span> app
+                </h1>
+                <p>Cred succulents literally, chambray coloring book messenger bag fanny pack lumbersexual shoreditch echo park sriracha semiotics helvetica mixtape.</p>
+                <button className='btn btn-hero'>Login/Register</button>
+            </div>
+            <img src={main} alt='job hunt' className='img main-img' />
+        </div>
+    </main>
+  );
 }
 
 export default Landing;
