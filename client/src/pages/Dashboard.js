51fbf49bca9cb5c7c15160c03dcbf6aaab3dabbc
Alexey Kovalenko <97091814+frost735@users.noreply.github.com>
2022-01-28 00:46:46 +0300

    ~

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,4 +1,19 @@
+import { useEffect } from "react";
+
 function Dashboard() {
+  const fetchData = async () => {
+    try {
+      const response = await fetch('http://localhost:4000')
+      const data = await response.json()
+      console.log(data)
+    } catch (error) {
+      
+    }
+    
+  }
+  useEffect(() => {
+    fetchData()
+  }, [])
   return <h1>Dashboard</h1>;
 }
 
