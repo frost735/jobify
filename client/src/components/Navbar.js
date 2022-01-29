9e2d11ee16d40dabc9d06683f6d7e185e464b6e5
frost735 <valorde90@gmail.com>
2022-01-29 06:12:21 +0300

    ~

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,9 +1,11 @@
 import Wrapper from '../assets/wrappers/Navbar'
+import { FaHome } from 'react-icons/fa'
 
 function Navbar() {
   return (
       <Wrapper>
           <h4>navbar</h4>
+          <FaHome/>
       </Wrapper>
   )
 }
