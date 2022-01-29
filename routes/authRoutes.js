9568e147fc94d0bd6e5b3b357c6f46c683703a23
frost735 <valorde90@gmail.com>
2022-01-29 22:44:34 +0300

    ~

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,10 +1,11 @@
 import expres from 'express'
 import { register, login, updateUser } from '../controllers/authController.js'
+import authenticateUser from '../middleware/auth.js'
 
 const router = expres.Router()
 
 router.route('/register').post(register)
 router.route('/login').post(login)
-router.route('/updateUser').patch(updateUser)
+router.route('/updateUser').patch(authenticateUser, updateUser)
 
 export default router
