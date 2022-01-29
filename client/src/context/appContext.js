1d93c4a06e8c652a1d51c986d44ba0332a10adae
frost735 <valorde90@gmail.com>
2022-01-29 08:25:11 +0300

    ~

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -4,15 +4,10 @@ import axios from 'axios'
 import { 
     DISPLAY_ALERT, 
     CLEAR_ALERT,
-    REGISTER_USER_BEGIN,
-    REGISTER_USER_SUCCESS,
-    REGISTER_USER_ERROR,
-    LOGIN_USER_BEGIN,
-    LOGIN_USER_SUCCESS,
-    LOGIN_USER_ERROR,
     SETUP_USER_BEGIN,
     SETUP_USER_SUCCESS,
-    SETUP_USER_ERROR, 
+    SETUP_USER_ERROR,
+    TOGGLE_SIDEBAR
 } from './actions'
 
 const token = localStorage.getItem('token')
@@ -28,6 +23,7 @@ const initialState = {
     token: token,
     userLocation: userLocation || '',
     jobLocation: userLocation || '',
+    showSidebar: false,
 }
 
 const AppContext = React.createContext()
@@ -58,44 +54,6 @@ const AppProvider = ({ children }) => {
         localStorage.removeItem('location')
     }
 
-    const registerUser = async (currentUser) => {
-        dispatch({ type: REGISTER_USER_BEGIN })
-        try {
-            const { data } = await axios.post('/api/v1/auth/register', currentUser)
-            const { user, token, location } = data
-            dispatch({ 
-                type: REGISTER_USER_SUCCESS, 
-                payload: { user, token, location },
-            })
-            addUserToLocalStorage({ user, token, location })
-        } catch (error) {
-            console.log(error.response)
-            dispatch({ 
-                type: REGISTER_USER_ERROR,
-                payload: { msg: error.response.data.msg },
-            })
-        }
-        clearAlert()
-    }
-    const loginUser = async (currentUser) => {
-        dispatch({ type: LOGIN_USER_BEGIN })
-        try {
-            const { data } = await axios.post('/api/v1/auth/login', currentUser)
-            const { user, token, location } = data
-            dispatch({ 
-                type: LOGIN_USER_SUCCESS, 
-                payload: { user, token, location },
-            })
-            addUserToLocalStorage({ user, token, location })
-        } catch (error) {
-            console.log(error.response)
-            dispatch({ 
-                type: LOGIN_USER_ERROR,
-                payload: { msg: error.response.data.msg },
-            })
-        }
-        clearAlert()
-    }
     const setupUser = async ({ currentUser, endPoint, alertText }) => {
         dispatch({ type: SETUP_USER_BEGIN })
         try {
@@ -115,14 +73,18 @@ const AppProvider = ({ children }) => {
         }
         clearAlert()
     }
+
+    const toggleSidebar = () => {
+        dispatch({ type: TOGGLE_SIDEBAR })
+    }
+
     return (
         <AppContext.Provider 
             value={{ 
                 ...state, 
-                displayAlert, 
-                registerUser,
-                loginUser,
-                setupUser
+                displayAlert,
+                setupUser,
+                toggleSidebar
             }}
         >
             {children}
