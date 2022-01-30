503929a2b1f1cf175236420add2bf9fd3ecf3fc4
frost735 <valorde90@gmail.com>
2022-01-30 06:07:40 +0300

    ~

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -84,6 +84,10 @@ const AppProvider = ({ children }) => {
         removeUserFromLocalStorage()
     }
 
+    const updateUser = async (currentUser) => {
+        console.log(currentUser)
+    }
+
     return (
         <AppContext.Provider 
             value={{ 
