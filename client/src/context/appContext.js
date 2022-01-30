061c55d137fbcc9ac11ba704b4d4756387c2e3ea
frost735 <valorde90@gmail.com>
2022-01-30 06:57:33 +0300

    ~

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -85,7 +85,16 @@ const AppProvider = ({ children }) => {
     }
 
     const updateUser = async (currentUser) => {
-        console.log(currentUser)
+        try {
+            const { data } =await axios.patch('/api/v1/auth/updateUser', currentUser, {
+                headers: {
+                    Authorization: `Bearer ${state.token}`
+                }
+            })
+            console.log(data)
+        } catch (error) {
+            console.log(error.response)
+        }
     }
 
     return (
@@ -94,6 +103,7 @@ const AppProvider = ({ children }) => {
                 ...state, 
                 displayAlert,
                 setupUser,
+                updateUser,
                 toggleSidebar,
                 logoutUser
             }}
